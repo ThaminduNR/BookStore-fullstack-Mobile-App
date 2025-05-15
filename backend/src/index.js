0a9edc6f1d95480b4dfc6cc61c0363d6c486b7e3
ThaminduNR <thaminduniwantha777@gmail.com>
2025-05-15 17:03:03 +0530

    create register route

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,6 +7,7 @@ const app = express();
 const PORT = process.env.PORT;
 
 app.use('/api/auth', authRoutes);
+app.use(express.json());
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,10 +1,52 @@
 import express from 'express';
+import User from '../models/User.js';
 
 const router = express.Router();
 
 router.post('/register', async (req, res) => {
-  res.send('Register Success');
+  try {
+    const { email, username, password } = req.body;
+    if (!email || !username || !password) {
+      return res.status(400).json({ message: 'All fields are required' });
+    }
+
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ message: 'Password must be at least 6 characters long' });
+    }
+    if (username.length < 3) {
+      return res
+        .status(400)
+        .json({ message: 'Username must be at least 3 characters long' });
+    }
+
+    // check if user already exists
+
+    const existingUsername = await User.findOne({ username });
+    if (existingUsername) {
+      return res.status(400).json({ message: 'Username already exists' });
+    }
+
+    const existingEmail = await User.findOne({ email });
+    if (existingEmail) {
+      return res.status(400).json({ message: 'Email already exists' });
+    }
+
+    //get random profile Image
+    const profileImage = `https://api.dicebear.com/7.x/pixel-art/svg?seed=${username}`;
+
+    const user = new User({
+      username,
+      email,
+      password,
+      profileImage,
+    });
+
+    const result = await user.save();
+  } catch (error) {}
 });
+
 router.post('/login', async (req, res) => {
   res.send('Login Success');
 });
